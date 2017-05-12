b04cd8c399442f6fd80b6ee4348b4a72362fa1cf
Amrik Singh <amrik.arethos@gmail.com>
2017-05-12 14:03:53 +0530

    LocalStorage Added

---
+++ client/src/Services/local.storage.ts
@@ -0,0 +1,45 @@
+import {Injectable, Inject} from '@angular/core';
+declare var jQuery: any;
+@Injectable()
+export class StorageService {
+    constructor(){
+        console.log("constructor: :  StorageService ");
+    }
+    contentComponentIdList: any=[];
+
+    add(key: string, value: any) {
+        console.log("key: ",key);
+        this.remove(key);
+        if (value) {
+            value = JSON.stringify(value);
+        }
+        localStorage.setItem(key, value);
+    }
+
+    get<T>(key: string): T {
+        let value: string = localStorage.getItem(key);
+
+        if (value && value !== "undefined" && value !== "null") {
+            try {
+                return <T>JSON.parse(value);
+            } catch (ex) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    remove(key: string) {
+        console.log("remove is called!!");
+        localStorage.removeItem(key);
+    }
+
+
+
+// theme change on dropdown------
+
+ // text style //
+
+ 
+
+}
